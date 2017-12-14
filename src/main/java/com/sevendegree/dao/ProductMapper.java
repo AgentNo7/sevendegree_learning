96798044c7e05c62da15a2f4d9ca471bf5ea9c10
AgentNo7 <littlesevenyhf@gmail.com>
2017-12-14 17:37:49 +0800

    project init commit

---
+++ src/main/java/com/sevendegree/dao/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.sevendegree.dao;
+
+import com.sevendegree.pojo.Product;
+
+public interface ProductMapper {
+    int deleteByPrimaryKey(Integer id);
+
+    int insert(Product record);
+
+    int insertSelective(Product record);
+
+    Product selectByPrimaryKey(Integer id);
+
+    int updateByPrimaryKeySelective(Product record);
+
+    int updateByPrimaryKey(Product record);
+}
